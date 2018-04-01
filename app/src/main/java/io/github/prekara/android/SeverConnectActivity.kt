b5343b0c1da17a632c88a93c30443f4b329ce47b
Life is Tech <lifeistech@S18-T102.local>
2018-04-01 11:17:43 +0900

    [add]ServerConnectActivity

--- app/src/main/java/io/github/prekara/android/SeverConnectActivity.kt
+++ app/src/main/java/io/github/prekara/android/SeverConnectActivity.kt
@@ -2,13 +2,11 @@ package io.github.prekara.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class SeverConnectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_sever_connect)
     }
 }

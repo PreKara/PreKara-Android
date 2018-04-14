6f0be2a27972f5e487d2c293c10293129bbff1b9
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2018-04-15 00:33:11 +0900

    [add] loop

--- app/src/main/java/io/github/prekara/android/Fragment/HomeTab/PresenterFragment.kt
+++ app/src/main/java/io/github/prekara/android/Fragment/HomeTab/PresenterFragment.kt
@@ -34,114 +34,54 @@ class PresenterFragment : Fragment() {
         return inflater!!.inflate(R.layout.fragment_presenter, container, false)
     }
 
+    var new = true;
+
+    var cookieStr = ""
     override fun onStart() {
         super.onStart()
 
-        val cookieStr = cookie.getString("cookie", "").replace("[", "").replace("]", "")
+        cookieStr = cookie.getString("cookie", "").replace("[", "").replace("]", "")
 //        if (!isSlideFinished) bt_next.text =_ "次のプレゼンターへ"
 
         bt_next.setOnClickListener {
-            "/control/new".httpGet().header("Content-Type" to "application/json").header("cookie" to cookieStr).responseJson { request, response, result ->
-                when (result) {
-                    is Result.Success -> {
-                        Log.d("Success", result.value.content)
-                        Toast.makeText(activity, "success", Toast.LENGTH_SHORT).show()
-                        bt_next.text = "次のスライドへ"
-                        bt_next.setOnClickListener {
-                            "/control/next".httpGet().header("Content-Type" to "application/json").header("cookie" to cookieStr).responseJson { request, response, result ->
-                                when (result) {
-                                    is Result.Success -> {
-                                        Log.d("Success", result.value.content)
-                                        Toast.makeText(activity, "success", Toast.LENGTH_SHORT).show()
-                                    }
-                                    is Result.Failure -> {
-                                        Log.d("Header", request.headers.toString())
-                                        result.getException().printStackTrace()
-                                    }
-                                }
-                            }
+            if(new) {
+                "/control/new".httpGet().header("Content-Type" to "application/json").header("cookie" to cookieStr).responseJson { request, response, result ->
+                    when (result) {
+                        is Result.Success -> {
+                            Log.d("Success", result.value.content)
+                            Toast.makeText(activity, "success", Toast.LENGTH_SHORT).show()
+                            bt_next.text = "次のスライドへ"
+                            new = false;
+                        }
+                        is Result.Failure -> {
+                            Log.d("Header", request.headers.toString())
+                            result.getException().printStackTrace()
                         }
                     }
-                    is Result.Failure -> {
-                        Log.d("Header", request.headers.toString())
-                        result.getException().printStackTrace()
+                }
+            }else {
+                "/control/next".httpGet().header("Content-Type" to "application/json").header("cookie" to cookieStr).responseJson { request, response, result ->
+                    when (result) {
+                        is Result.Success -> {
+                            Log.d("Success", result.value.content)
+                            Toast.makeText(activity, "success", Toast.LENGTH_SHORT).show()
+                        }
+                        is Result.Failure -> {
+                            if (result.error.response.statusCode == 405) {
+
+                                Toast.makeText(activity, "finished", Toast.LENGTH_SHORT).show()
+                                bt_next.text = "次のプレゼンターへ"
+                                new = true;
+                            }
+
+                            Log.d("Header", request.headers.toString())
+                            result.getException().printStackTrace()
+                        }
                     }
                 }
             }
-
-//            bt_next.setOnClickListener {
-//
-//                val nextSlide = NextSlide()
-//                nextSlide.setListener(object : AsyncNextListener {
-//                    override fun preExecute() {
-//                        // Nothing
-//                    }
-//
-//                    override fun postExecute() {
-//
-//                    }
-//
-//                    override fun doInBackground(): String {
-//                        if (isTimeUp) {
-//                            "/control/new".httpGet()
-//                                    .header("Content-Type" to "application/json")
-//                                    .header("Cookie" to cookie.getString("cookie", ""))
-//                                    .responseJson { request, response, result ->
-//                                        when (result) {
-//                                            is Result.Success -> {
-//                                                // Success
-//                                                Log.d("NextPresenter", "${result.value}")
-//                                            }
-//                                            is Result.Failure -> {
-//                                                // Error
-//                                                Log.e("Error at Presenter", result.getException().toString())
-//                                            }
-//                                        }
-//                                    }
-//                        } else {
-//                            if (isSlideFinished) {
-//                                "/control/new".httpGet()
-//                                        .header("Content-Type" to "application/json")
-//                                        .header("Cookie" to cookie.getString("cookie", ""))
-//                                        .response()
-//                                isSlideFinished = false
-//                                return ""
-//                            }
-//
-//                            var triple = "/control/next".httpGet()
-//                                    .header("Content-Type" to "application/json")
-//                                    .header("Cookie" to cookie.getString("cookie", ""))
-//                                    .responseJson()
-//                            when (triple.third) {
-//                                is Result.Success -> {
-//                                    Log.d("NextSlide", triple.third.toString())
-//                                }
-//
-//
-//                            }
-//                        }
-////                                .responseJson { request, response, result ->
-////                                    when (result) {
-////                                        is Result.Success -> {
-////                                            // Success
-////                                            Log.d("NextSlide", "${result.value}")
-////                                        }
-////                                        is Result.Failure -> {
-////                                            // Error
-////                                            result.getException().printStackTrace()
-//////                                            Log.e("Error", result.getException().toString())
-////                                        }
-////                                    }
-//
-//
-////                }
-//                        return ""
-//                    }
-//
-//                })
-//                nextSlide.execute()
-//            }
-
         }
     }// Required empty public constructor
+
+//    val new = View.OnClickListener{
 }
